a33e0ad6906f7b3fd9a4fc17e2310e33601b7265
kirthi1594 <kirthikajayaraman1594@gmail.com>
2024-02-14 00:45:37 +0530

    updated

--- client1/src/pages/Admin/Products.js
+++ client1/src/pages/Admin/Products.js
@@ -39,7 +39,7 @@ const Products = () => {
               >
                 <div className="card m-2" style={{ width: "18rem" }}>
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://asos2.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
